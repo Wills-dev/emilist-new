6387c6ead18e45e4cff9664bd23c6fe2d96d0d1e
wills-dev <ejioguvictor6@gmail.com>
2024-09-12 15:10:23 +0100

    feat: dashboard layout

--- hooks/useFetchMaterials.ts
+++ hooks/useFetchMaterials.ts
@@ -1,31 +1,94 @@
-import { useQuery } from "react-query";
+import { ChangeEvent, useEffect, useMemo, useState } from "react";
 
 import { axiosInstance } from "@/axiosInstance/baseUrl";
 
 export const useFetchMaterials = () => {
-  const onError = (error: any) => {
-    if (error) {
-      console.log("error fetching materials", error);
+  const [allMaterials, setAllMaterials] = useState<any>([]);
+  const [loading, setLoading] = useState<boolean>(true);
+  const [currentPage, setCurrentPage] = useState(1);
+  const [totalPages, setTotalPages] = useState(1);
+  const [search, setSearch] = useState("");
+  const ITEMS_PER_PAGE = 10;
+
+  const handleChange = (e: ChangeEvent<HTMLInputElement>) => {
+    setSearch(e.target.value);
+  };
+
+  const handlePageChange = (newPage: number) => {
+    setCurrentPage(newPage);
+  };
+
+  const getAllMaterials = async () => {
+    try {
+      const data = await axiosInstance.get(`/fetchMaterials`);
+      setAllMaterials(data?.data);
+      const totalJobs = data?.data?.length;
+      setTotalPages(Math.ceil(totalJobs / ITEMS_PER_PAGE));
+    } catch (error: any) {
+      console.log("error fetching all materials", error);
+    } finally {
+      setLoading(false);
     }
   };
 
-  const getAllMaterials = () => {
-    return axiosInstance.get("/fetchMaterials").then((res) => res?.data);
-  };
+  useEffect(() => {
+    getAllMaterials();
+  }, []);
 
-  const { data: allMaterials, isLoading: loading } = useQuery(
-    "all materials",
-    getAllMaterials,
-    {
-      onError: onError,
-      staleTime: 60 * 60 * 1000,
-      cacheTime: 60 * 60 * 1000,
-      refetchOnMount: false,
-      refetchOnWindowFocus: false,
+  const allMaterialsData = useMemo(() => {
+    let computedMaterials = allMaterials;
+
+    if (search) {
+      computedMaterials = computedMaterials?.filter(
+        (material: any) =>
+          material?.businessname
+            ?.toLowerCase()
+            .includes(search.toLowerCase()) ||
+          material?.country?.toLowerCase().includes(search.toLowerCase()) ||
+          material?.firstname?.toLowerCase().includes(search.toLowerCase()) ||
+          material?.service?.toLowerCase().includes(search.toLowerCase()) ||
+          material?.contactpersonName
+            ?.toLowerCase()
+            .includes(search.toLowerCase()) ||
+          material?.city?.toLowerCase().includes(search.toLowerCase())
+      );
     }
-  );
+
+    return computedMaterials?.slice(
+      (currentPage - 1) * ITEMS_PER_PAGE,
+      (currentPage - 1) * ITEMS_PER_PAGE + ITEMS_PER_PAGE
+    );
+  }, [allMaterials, currentPage, search]);
+
+  // const onError = (error: any) => {
+  //   if (error) {
+  //     console.log("error fetching materials", error);
+  //   }
+  // };
+
+  // const getAllMaterials = () => {
+  //   return axiosInstance.get("/fetchMaterials").then((res) => res?.data);
+  // };
+
+  // const { data: allMaterials, isLoading: loading } = useQuery(
+  //   "all materials",
+  //   getAllMaterials,
+  //   {
+  //     onError: onError,
+  //     staleTime: 60 * 60 * 1000,
+  //     cacheTime: 60 * 60 * 1000,
+  //     refetchOnMount: false,
+  //     refetchOnWindowFocus: false,
+  //   }
+  // );
   return {
     loading,
     allMaterials,
+    allMaterialsData,
+    search,
+    handleChange,
+    handlePageChange,
+    totalPages,
+    currentPage,
   };
 };
